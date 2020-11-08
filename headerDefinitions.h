e87114fa95b9920e00933e592307917ef38c3173
ecn <vinayakpkumar47@gmail.com>
2020-11-08 14:36:05 +0100

    Adding algorithm for average computation time and to detect deadline overruns

--- headerDefinitions.h
+++ headerDefinitions.h
@@ -3,13 +3,21 @@
 #include<iostream>
 #include <vector>
 #include <string>
-
+using namespace std;
 struct taskInfo_st
 {
     uint8_t taskId_u8;
     uint16_t period_u16;
     uint16_t computationTime_u16;
+    uint16_t arrivalTime_u16;
+    uint16_t deadline_u16;
     bool operator() (taskInfo_st i,taskInfo_st j) { return (i.period_u16<j.period_u16);}  //Operator to perform sorting within a vector structure.
    } sortWaitingList;
+struct taskMetrics_st
+{
+    uint16_t taskInstance = 0;
+    vector<uint16_t>responseTimes_au16;
+    vector<uint16_t>completionTime_au16;
+};
 
 #endif
--- mainFile.cpp
+++ mainFile.cpp
@@ -14,6 +14,7 @@ vector <taskInfo_st> taskDetails_ast;
 uint8_t numberOfTasks_u8 = 0;
 vector <taskInfo_st> readyTasks_ast;
 uint8_t tasksReady_u8 = 0;
+vector <taskMetrics_st> metrics_ast;
 /***********************Local functions*******************************************************************/
 static void simulator();
 
@@ -23,6 +24,7 @@ int main(void)
     float U_ft = 0;  //Variable to calculate the Utilization factor
     uint16_t hyperperiod_u16 = 0; //Variable to calculate hyperperiod
     vector<uint16_t> taskPeriods_au16; //Variable to store periods (Used while calculating hyperperiod) TODO: Try to improve
+    float averageResponseTime_ft = 0;
     /*Get number of tasks*/
     printf("Enter the number of tasks\n");
     scanf("%hhu",&numberOfTasks_u8);
@@ -32,8 +34,10 @@ int main(void)
     for(int i = 0; i<numberOfTasks_u8;i++)
     {
         taskDetails_ast.push_back(taskInfo_st());
+        metrics_ast.push_back(taskMetrics_st());
         taskPeriods_au16.push_back(uint16_t());
         scanf("%hu",&taskDetails_ast[i].period_u16);
+        taskDetails_ast[i].deadline_u16 = taskDetails_ast[i].period_u16;
         taskPeriods_au16[i] = taskDetails_ast[i].period_u16;  //Copy onto local variable
         taskDetails_ast[i].taskId_u8 = i;
     }
@@ -64,7 +68,7 @@ int main(void)
         U_ft = U_ft + ((float)(taskDetails_ast[i].computationTime_u16))/((float)(taskDetails_ast[i].period_u16));
     }
     printf("U = %f \n",U_ft);
-    if(U_ft >= 1)
+    if(U_ft > 1)
     {
         printf("ERROR: Tasks not schedulable\n");
         exit(1);
@@ -73,12 +77,15 @@ int main(void)
     printf("Hyperperiod = %hu \n",hyperperiod_u16);
     for(int i = 0; i<hyperperiod_u16;i++)
         simulator();
+    for(int i = 0;i<numberOfTasks_u8;i++)
+    {
+        for(int j = 0;j<metrics_ast[i].responseTimes_au16.size();j++)
+            averageResponseTime_ft += metrics_ast[i].responseTimes_au16[j];
+        averageResponseTime_ft = (float)averageResponseTime_ft/(float)(metrics_ast[i].responseTimes_au16.size());
+        cout<<"Average response time of task "<<i<<" is "<<averageResponseTime_ft<<endl;
+        averageResponseTime_ft = 0;
+    }
 
-//    for(int i = 0; i<tasksReady_u8;i++)
-//    {
-//        printf("Task ID : %hhu is in the ready list\n",taskList_au8[i]);
-//    }
-        //simulator();
     return 1;
 }
 
@@ -92,19 +99,31 @@ static void simulator()
         {
             readyTasks_ast.push_back(taskInfo_st());
             readyTasks_ast[tasksReady_u8] = taskDetails_ast[i];
+            readyTasks_ast[tasksReady_u8].arrivalTime_u16 = systemTime_u16;
+            readyTasks_ast[tasksReady_u8].deadline_u16+=systemTime_u16;
             tasksReady_u8++;
         }
     }
     std::sort(readyTasks_ast.begin(), readyTasks_ast.end(), sortWaitingList);
-//     for(int i = 0; i<numberOfTasks_u8;i++)
-//        cout<<"Task "<<(unsigned)readyTasks_ast[i].taskId_u8<<" with period "<<readyTasks_ast[i].period_u16<<" at index "<<i<<endl;
+
     if(tasksReady_u8 > 0)
     {
+        for(int i = 0;i<tasksReady_u8;i++)
+        {
+            if(readyTasks_ast[i].deadline_u16 <= systemTime_u16)
+            {
+                cout<<"Schedule unfeasible. Deadline of task "<<(unsigned)readyTasks_ast[i].taskId_u8<<" missed. System time: "<<systemTime_u16<<endl;
+                exit(-1);
+            }
+        }
         cout<<"System Time: "<<systemTime_u16<<" Scheduling task "<<(unsigned)readyTasks_ast[0].taskId_u8<<" with period"<<readyTasks_ast[0].period_u16<<endl;
         readyTasks_ast[0].computationTime_u16--;
         if(readyTasks_ast[0].computationTime_u16 == 0)
         {
             tasksReady_u8--;
+            metrics_ast[readyTasks_ast[0].taskId_u8].responseTimes_au16.push_back(uint16_t());
+            metrics_ast[readyTasks_ast[0].taskId_u8].responseTimes_au16[metrics_ast[readyTasks_ast[0].taskId_u8].taskInstance] = systemTime_u16 - readyTasks_ast[0].arrivalTime_u16 + 1;
+            metrics_ast[readyTasks_ast[0].taskId_u8].taskInstance++;
             readyTasks_ast.erase(readyTasks_ast.begin());
         }
     }
